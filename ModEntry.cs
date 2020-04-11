2446150121e86202ebcfbb1fb48366e1fb2f99f2
ZaneYork <ZaneYork@qq.com>
2020-04-11 20:23:00 +0800

    Change patch mode to make it available for other platform

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,13 +1,13 @@
+using System;
 using System.Collections.Generic;
-using System.Reflection;
-using Android.OS;
 using Harmony;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI.Mods.CustomLocalization.Rewrites;
 using StardewValley;
 using StardewValley.BellsAndWhistles;
 using StardewValley.Menus;
-using StardewValley.Mobile;
 using StardewValley.Objects;
 using StardewValley.Projectiles;
 
@@ -21,6 +21,8 @@ namespace StardewModdingAPI.Mods.CustomLocalization
 
         public static string ModPath;
 
+        public static IReflectionHelper Reflection;
+
         private static ModEntry Instance;
 
         public static void SaveConfig()
@@ -30,14 +32,12 @@ namespace StardewModdingAPI.Mods.CustomLocalization
 
         public override void Entry(IModHelper helper)
         {
-            if (Build.VERSION.SdkInt < BuildVersionCodes.M)
-                return;
             Instance = this;
             ModConfig = helper.ReadConfig<ModConfig>();
             ModPath = helper.DirectoryPath;
             monitor = this.Monitor;
-            HarmonyInstance harmony = HarmonyInstance.Create("zaneyork.CustomLocalization");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Reflection = helper.Reflection;
+            DoPatch();
 
             Dictionary<string, bool> dictionary = this.Helper.Reflection.GetField<Dictionary<string, bool>>(Game1.content, "_localizedAsset").GetValue();
             Dictionary<string, object> dictionaryAssets = this.Helper.Reflection.GetField<Dictionary<string, object>>(Game1.content, "loadedAssets").GetValue();
@@ -55,6 +55,66 @@ namespace StardewModdingAPI.Mods.CustomLocalization
                 {
                     this.Helper.Reflection.GetMethod(typeof(SpriteText), "OnLanguageChange").Invoke(LocalizedContentManager.CurrentLanguageCode);
                 }
+            }
+        }
+
+        private static void DoPatch()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("zaneyork.CustomLocalization");
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(TitleContainer), "OpenStream"),
+                prefix: new HarmonyMethod(typeof(TitleContainerRewrites.OpenStreamRewrite), nameof(TitleContainerRewrites.OpenStreamRewrite.Prefix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(StartupPreferences), "readSettings"),
+                postfix: new HarmonyMethod(typeof(StartupPreferencesRewrites.ReadSettingsRewrite), nameof(StartupPreferencesRewrites.ReadSettingsRewrite.Postfix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(StartupPreferences), "writeSettings"),
+                prefix: new HarmonyMethod(typeof(StartupPreferencesRewrites.WriteSettingsRewrite), nameof(StartupPreferencesRewrites.WriteSettingsRewrite.Prefix)),
+                postfix: new HarmonyMethod(typeof(StartupPreferencesRewrites.WriteSettingsRewrite), nameof(StartupPreferencesRewrites.WriteSettingsRewrite.Postfix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(StartupPreferences), "writeSettings"),
+                prefix: new HarmonyMethod(typeof(StartupPreferencesRewrites.WriteSettingsRewrite), nameof(StartupPreferencesRewrites.WriteSettingsRewrite.Prefix)),
+                postfix: new HarmonyMethod(typeof(StartupPreferencesRewrites.WriteSettingsRewrite), nameof(StartupPreferencesRewrites.WriteSettingsRewrite.Postfix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(SpriteText), "OnLanguageChange"),
+                prefix: new HarmonyMethod(typeof(SpriteTextRewrites.OnLanguageChangeRewrite), nameof(SpriteTextRewrites.OnLanguageChangeRewrite.Prefix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(SpriteText), "setUpCharacterMap"),
+                prefix: new HarmonyMethod(typeof(SpriteTextRewrites.SetUpCharacterMapRewrite), nameof(SpriteTextRewrites.SetUpCharacterMapRewrite.Prefix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredMethod(typeof(LocalizedContentManager), "LanguageCodeString"),
+                prefix: new HarmonyMethod(typeof(LocalizedContentManagerRewrites.LanguageCodeStringRewrite), nameof(LocalizedContentManagerRewrites.LanguageCodeStringRewrite.Prefix))
+            );
+            harmony.Patch(
+                original: AccessTools.DeclaredProperty(typeof(LocalizedContentManager), "CurrentLanguageLatin").GetMethod,
+                prefix: new HarmonyMethod(typeof(LocalizedContentManagerRewrites.GetCurrentLanguageLatinRewrite), nameof(LocalizedContentManagerRewrites.GetCurrentLanguageLatinRewrite.Prefix))
+            );
+            
+            Type mobileMenuType = AccessTools.TypeByName("StardewValley.Menus.LanguageSelectionMobile");
+            if (mobileMenuType != null)
+            {
+                harmony.Patch(
+                    original: AccessTools.DeclaredMethod(mobileMenuType, "draw", new Type[] { typeof(SpriteBatch) }),
+                    prefix: new HarmonyMethod(typeof(LanguageSelectionMobileRewrites.DrawRewrite), nameof(LanguageSelectionMobileRewrites.DrawRewrite.Prefix))
+                );
+                harmony.Patch(
+                    original: AccessTools.DeclaredMethod(mobileMenuType, "releaseLeftClick"),
+                    prefix: new HarmonyMethod(typeof(LanguageSelectionMobileRewrites.ReleaseLeftClickRewrite), nameof(LanguageSelectionMobileRewrites.ReleaseLeftClickRewrite.Prefix))
+                );
+                harmony.Patch(
+                    original: AccessTools.DeclaredMethod(mobileMenuType, "setCurrentItemIndex"),
+                    prefix: new HarmonyMethod(typeof(LanguageSelectionMobileRewrites.SetCurrentItemIndexRewrite), nameof(LanguageSelectionMobileRewrites.SetCurrentItemIndexRewrite.Prefix))
+                );
+                harmony.Patch(
+                    original: AccessTools.DeclaredMethod(mobileMenuType, "SetupButtons"),
+                    transpiler: new HarmonyMethod(typeof(LanguageSelectionMobileRewrites.SetupButtonsRewrite), nameof(LanguageSelectionMobileRewrites.SetupButtonsRewrite.Transpiler))
+                );
             }
         }
 
@@ -77,17 +137,26 @@ namespace StardewModdingAPI.Mods.CustomLocalization
             Game1.giftboxTexture = ((ContentManager)Game1.content).Load<Texture2D>("LooseSprites\\Giftbox");
             Game1.controllerMaps = ((ContentManager)Game1.content).Load<Texture2D>("LooseSprites\\ControllerMaps");
             Game1.animations = ((ContentManager)Game1.content).Load<Texture2D>("TileSheets\\animations");
-            Game1.mobileSpriteSheet = ((ContentManager)Game1.content).Load<Texture2D>("LooseSprites\\MobileAtlas_manually_made");
+            Reflection.GetField<Texture2D>(typeof(Game1), "mobileSpriteSheet", false)?.SetValue(((ContentManager)Game1.content).Load<Texture2D>("LooseSprites\\MobileAtlas_manually_made"));
             Game1.achievements = (Dictionary<int, string>)((ContentManager)Game1.content).Load<Dictionary<int, string>>("Data\\Achievements");
             Game1.NPCGiftTastes = (IDictionary<string, string>)((ContentManager)Game1.content).Load<Dictionary<string, string>>("Data\\NPCGiftTastes");
             Game1.onScreenMenus.Clear();
-            this.Helper.Reflection.GetMethod(Game1.game1, "TranslateFields").Invoke();
+            Reflection.GetMethod(Game1.game1, "TranslateFields").Invoke();
             Game1.dayTimeMoneyBox = new DayTimeMoneyBox();
-            Game1.dayTimeMoneyBox.game1 = Game1.game1;
+            Reflection.GetField<Game1>(Game1.dayTimeMoneyBox, "game1", false)?.SetValue(Game1.game1);
             Game1.onScreenMenus.Add((IClickableMenu)Game1.dayTimeMoneyBox);
-            Game1.toolbar = new Toolbar();
-            Game1.virtualJoypad = new VirtualJoypad();
-            Game1.onScreenMenus.Add((IClickableMenu)Game1.toolbar);
+            IReflectedField<Toolbar> fieldToolBar = Helper.Reflection.GetField<Toolbar>(typeof(Game1), "toolbar", false);
+            fieldToolBar?.SetValue(new Toolbar());
+            Type typeVirtualJoypad = AccessTools.TypeByName("StardewValley.Mobile.VirtualJoypad");
+            if(typeVirtualJoypad != null)
+            {
+                object virtualJoypad = AccessTools.CreateInstance(typeVirtualJoypad);
+                Reflection.GetField<object>(typeof(Game1), "virtualJoypad", false)?.SetValue(virtualJoypad);
+            }
+            if (fieldToolBar != null)
+            {
+                Game1.onScreenMenus.Add(fieldToolBar.GetValue());
+            }
             Game1.buffsDisplay = new BuffsDisplay();
             Game1.onScreenMenus.Add((IClickableMenu)Game1.buffsDisplay);
             Game1.dialogueFont = (SpriteFont)((ContentManager)Game1.content).Load<SpriteFont>("Fonts\\SpriteFont1");
